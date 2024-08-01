6d68f2bed52ac42ee23200e866de41535def66f0
maheshistudy <maheshi.study@gmail.com>
2024-08-02 01:02:10 +1200

    Draw Wallpaper 7

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -3,7 +3,7 @@ let drawAStarAtTop = true;  // if draw a star at the top of the tree
 let drawSnowflakes = true;  // if draw snowflakes on the tree
 let drawBulbs = true;  // if draw bulbs on the tree
 let addGiftBox = true; // if draw gift boxes on the background
-let addCherry = true;  // if draw cherries on the background
+let addCherry = false;  // if draw cherries on the background
 let snowflakeWidth = 5; // snowflake width
 let snowflakeHeight = 5;  // snoflake height
 
@@ -29,7 +29,7 @@ function wallpaper_background() {
   let coolblue = color(139, 193, 232);
   let purple = color(108, 32, 189);
   let magenta = color(115, 6, 82);
-  background(white); // white background
+  background(magenta); // magenta background
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
